bee28b1aebc3ddc102137d3951fe217704dfa0f0
Jesse Smart <jsmartdev@protonmail.com>
2021-08-12 19:03:09 -0400

    fixed typo in object key

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 /* eslint-disable */
+
+// an object key to translate each letter to its corresponding number
+
+const key = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 1,
+    k: 2,
+    l: 3,
+    m: 4,
+    n: 5,
+    o: 6,
+    p: 7,
+    q: 8,
+    r: 9,
+    s: 1,
+    t: 2,
+    u: 3,
+    v: 4,
+    w: 5,
+    x: 6,
+    y: 7,
+    z: 8
+}
+
+
+
