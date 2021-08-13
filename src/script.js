a4907b2f62fff522f597661c83c5a1994b16a8dd
Jesse Smart <jsmartdev@protonmail.com>
2021-08-13 15:13:44 -0400

    fixed key object

--- src/script.js
+++ src/script.js
@@ -3,33 +3,43 @@
 // an object key to translate each letter to its corresponding number
 
 const key = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-    e: 5,
-    f: 6,
-    g: 7,
-    h: 8,
-    i: 9,
-    j: 1,
-    k: 2,
-    l: 3,
-    m: 4,
-    n: 5,
-    o: 6,
-    p: 7,
-    q: 8,
-    r: 9,
-    s: 1,
-    t: 2,
-    u: 3,
-    v: 4,
-    w: 5,
-    x: 6,
-    y: 7,
-    z: 8
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 1,
+    "k": 2,
+    "l": 3,
+    "m": 4,
+    "n": 5,
+    "o": 6,
+    "p": 7,
+    "q": 8,
+    "r": 9,
+    "s": 1,
+    "t": 2,
+    "u": 3,
+    "v": 4,
+    "w": 5,
+    "x": 6,
+    "y": 7,
+    "z": 8
 }
+
+/* Calculation Functions */
+
+/* document.getElementById("text").addEventListener("input", function() {
+    let str = this.value.toLowerCase();
+    let sum = [...str].map(c => numbers[c] || 0).reduce((a, b) => a + b, 0);
+    console.log(sum);
+}); */
+
+
 
 //  calculate life path number
 
@@ -67,3 +77,16 @@ function destinyNum() {
 
 }
 
+/* Template Functions */
+
+
+
+/* Render Functions */
+
+
+
+/* Event Handlers */
+
+
+
+/* Main Function */
